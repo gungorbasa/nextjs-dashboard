6afbbe40abcf3396255af7047e0d4fa8021365eb
Güngör <gungorbasa@gmail.com>
2024-02-19 13:12:01 +0300

    Finish Chapter 15
    Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -27,6 +29,25 @@ const fromScheme = z.object({
 
 const CreateInvoice = fromScheme.omit({ id: true, date: true });
 const UpdateInvoice = fromScheme.omit({ id: true, date: true });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 export async function createInvoice(previousState: State, formData: FormData) {
   try {
