e0c8174c4a8350caf0d713c07bf5b8d460275a49
Güngör <gungorbasa@gmail.com>
2024-02-19 13:27:03 +0300

    Finish Chapter 16
    Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,18 @@
-import CardWrapper from "@/app/ui/dashboard/cards";
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { lusitana } from "@/app/ui/fonts";
-import { Suspense } from "react";
-import { 
-  RevenueChartSkeleton, 
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-  CardsSkeleton
-} from "@/app/ui/skeletons";
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
